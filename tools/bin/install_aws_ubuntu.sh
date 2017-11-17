05d5d095d12b607a66da18e5945256ee6d6946f0
Manuel Ignacio Franco Galeano <maigfrga@gmail.com>
2017-11-17 15:04:34 +0000

    install aws

--- tools/bin/install_aws_ubuntu.sh
+++ tools/bin/install_aws_ubuntu.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade -y
 sudo apt-get install python3-matplotlib -y
 sudo apt-get install python3-pip -y
 sudo apt-get install git build-essential htop g++ gcc vim libssl-dev libffi-dev python3-dev  -y
-sudo python3 -m nltk.downloader all
 sudo pip3 install -r ~/src/nt-recommend/tools/requirements/base.txt
+sudo python3 -m nltk.downloader all
