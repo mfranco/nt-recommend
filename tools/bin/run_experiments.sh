455e68b2314bc1fa1f7b3042abc7fe523c04864a
Manuel Ignacio Franco Galeano <maigfrga@gmail.com>
2017-11-17 15:15:07 +0000

    aws experiments

--- tools/bin/run_experiments.sh
+++ tools/bin/run_experiments.sh
@@ -3,5 +3,9 @@
 cd ~/src/nt-recommend/src
 python3 manage.py resnik_collaborative_filter  --m msd --ns 10 >> ~/src/nt-recommend/data/results/resnik.txt
 python3 manage.py resnik_collaborative_filter  --m pearson --ns 10 >> ~/src/nt-recommend/data/results/resnik.txt
+
+sleep 5
 python3 manage.py resnik_collaborative_filter  --m msd --ns 100 >> ~/src/nt-recommend/data/results/resnik.txt
+sleep 5
 python3 manage.py resnik_collaborative_filter  --m pearson --ns 100 >> ~/src/nt-recommend/data/results/resnik.txt
+sleep 5
