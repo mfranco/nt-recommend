65bc63d408549d835a222feb21750f6381eeef4d
Manuel Ignacio Franco Galeano <maigfrga@gmail.com>
2017-11-17 10:35:12 +0000

    provision aws

---
+++ tools/bin/install_aws_ubuntu.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt-get install python3-matplotlib
+sudo apt-get install python3-pip
+sudo apt-get install git build-essential g++ gcc vim libssl-dev libffi-dev python3-dev  -y
+sudo pip3 install -r ~/src/nt-recommend/tools/requirements/base.txt
