206c22e4467082cec41b1ad0ab5397a38573601c
Manuel Ignacio Franco Galeano <maigfrga@gmail.com>
2017-11-21 14:35:33 +0000

    generator kfold

--- tools/bin/install_aws_ubuntu.sh
+++ tools/bin/install_aws_ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install python3-matplotlib -y
+sudo apt-get install python3-setuptools python3-matplotlib -y
 sudo apt-get install python3-pip -y
 sudo apt-get install git build-essential htop g++ gcc vim libssl-dev libffi-dev python3-dev  -y
 sudo pip3 install -r ~/src/nt-recommend/tools/requirements/base.txt
--- tools/bin/run_experiments.sh
+++ tools/bin/run_experiments.sh
@@ -1,57 +1,57 @@
 #!/bin/bash
-
+mkdir -p ~/src/nt-recommend/data/results/
 cd ~/src/nt-recommend/src
 
 # mean predictor
 
-python manage.py mean_predictor >> ~/src/nt-recommend/data/results/results.txt
+python3 manage.py mean_predictor >> ~/src/nt-recommend/data/results/results.txt
 sleep 10
 
-python manage.py mean_predictor --kn 10 >> ~/src/nt-recommend/data/results/results.txt
+python3 manage.py mean_predictor --kn 10 >> ~/src/nt-recommend/data/results/results.txt
 sleep 10
 
 
 # collaborative filtering
 
-python manage.py collaborative_filter --m msd --ns 10 >> ~/src/nt-recommend/data/results/results.txt
+python3 manage.py collaborative_filter --m msd --ns 10 >> ~/src/nt-recommend/data/results/results.txt
 sleep 10
 
-python manage.py collaborative_filter --m pearson --ns 10 >> ~/src/nt-recommend/data/results/results.txt
-sleep 10
-
-
-python manage.py collaborative_filter --m msd --ns 10 --kn 10 >> ~/src/nt-recommend/data/results/results.txt
-sleep 10
-
-python manage.py collaborative_filter --m pearson --ns 10 --kn 10 >> ~/src/nt-recommend/data/results/results.txt
+python3 manage.py collaborative_filter --m pearson --ns 10 >> ~/src/nt-recommend/data/results/results.txt
 sleep 10
 
 
-python manage.py collaborative_filter --m msd --ns 100 --kn 10 >> ~/src/nt-recommend/data/results/results.txt
+python3 manage.py collaborative_filter --m msd --ns 10 --kn 10 >> ~/src/nt-recommend/data/results/results.txt
 sleep 10
 
-python manage.py collaborative_filter --m pearson --ns 100 --kn 10 >> ~/src/nt-recommend/data/results/results.txt
+python3 manage.py collaborative_filter --m pearson --ns 10 --kn 10 >> ~/src/nt-recommend/data/results/results.txt
+sleep 10
+
+
+python3 manage.py collaborative_filter --m msd --ns 100 --kn 10 >> ~/src/nt-recommend/data/results/results.txt
+sleep 10
+
+python3 manage.py collaborative_filter --m pearson --ns 100 --kn 10 >> ~/src/nt-recommend/data/results/results.txt
 sleep 10
 
 
 # resnik collaborative filtering
 
 
-python manage.py resnik_collaborative_filter  --m msd --ns 10 >> ~/src/nt-recommend/data/results/results.txt
+python3 manage.py resnik_collaborative_filter  --m msd --ns 10 >> ~/src/nt-recommend/data/results/results.txt
 sleep 10
 
-python manage.py resnik_collaborative_filter  --m pearson --ns 10 >> ~/src/nt-recommend/data/results/results.txt
+python3 manage.py resnik_collaborative_filter  --m pearson --ns 10 >> ~/src/nt-recommend/data/results/results.txt
 sleep 10
 
-python manage.py resnik_collaborative_filter  --m pearson --ns 10 --kn 10 >> ~/src/nt-recommend/data/results/results.txt
+python3 manage.py resnik_collaborative_filter  --m pearson --ns 10 --kn 10 >> ~/src/nt-recommend/data/results/results.txt
 sleep 10
 
 
-python manage.py resnik_collaborative_filter  --m msd --ns 100 >> ~/src/nt-recommend/data/results/results.txt
+python3 manage.py resnik_collaborative_filter  --m msd --ns 100 >> ~/src/nt-recommend/data/results/results.txt
 sleep 10
 
-python manage.py resnik_collaborative_filter  --m pearson --ns 100 >> ~/src/nt-recommend/data/results/results.txt
+python3 manage.py resnik_collaborative_filter  --m pearson --ns 100 >> ~/src/nt-recommend/data/results/results.txt
 sleep 10
 
-python manage.py resnik_collaborative_filter  --m pearson --ns 100 --kn 10 >> ~/src/nt-recommend/data/results/results.txt
+python3 manage.py resnik_collaborative_filter  --m pearson --ns 100 --kn 10 >> ~/src/nt-recommend/data/results/results.txt
 sleep 10
