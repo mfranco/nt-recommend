d4dc0699ef9657198ca2ec6d03e65e70d815b84a
Manuel Ignacio Franco Galeano <maigfrga@gmail.com>
2017-11-21 16:59:56 +0000

    run experiments 2

--- tools/bin/run_experiments.sh
+++ tools/bin/run_experiments.sh
@@ -37,21 +37,21 @@ sleep 10
 # resnik collaborative filtering
 
 
-python3 manage.py resnik_collaborative_filter  --m msd --ns 10 >> ~/src/nt-recommend/data/results/results.txt
-sleep 10
-
-python3 manage.py resnik_collaborative_filter  --m pearson --ns 10 >> ~/src/nt-recommend/data/results/results.txt
-sleep 10
-
-python3 manage.py resnik_collaborative_filter  --m pearson --ns 10 --kn 10 >> ~/src/nt-recommend/data/results/results.txt
-sleep 10
-
-
-python3 manage.py resnik_collaborative_filter  --m msd --ns 100 >> ~/src/nt-recommend/data/results/results.txt
-sleep 10
-
-python3 manage.py resnik_collaborative_filter  --m pearson --ns 100 >> ~/src/nt-recommend/data/results/results.txt
-sleep 10
-
-python3 manage.py resnik_collaborative_filter  --m pearson --ns 100 --kn 10 >> ~/src/nt-recommend/data/results/results.txt
-sleep 10
+#python3 manage.py resnik_collaborative_filter  --m msd --ns 10 >> ~/src/nt-recommend/data/results/results.txt
+#sleep 10
+#
+#python3 manage.py resnik_collaborative_filter  --m pearson --ns 10 >> ~/src/nt-recommend/data/results/results.txt
+#sleep 10
+#
+#python3 manage.py resnik_collaborative_filter  --m pearson --ns 10 --kn 10 >> ~/src/nt-recommend/data/results/results.txt
+#sleep 10
+#
+#
+#python3 manage.py resnik_collaborative_filter  --m msd --ns 100 >> ~/src/nt-recommend/data/results/results.txt
+#sleep 10
+#
+#python3 manage.py resnik_collaborative_filter  --m pearson --ns 100 >> ~/src/nt-recommend/data/results/results.txt
+#sleep 10
+#
+#python3 manage.py resnik_collaborative_filter  --m pearson --ns 100 --kn 10 >> ~/src/nt-recommend/data/results/results.txt
+#sleep 10
