a11d39a4fa133f63aa5f9d7ab0ecb80e39c61742
Manuel Ignacio Franco Galeano <maigfrga@gmail.com>
2017-11-28 11:16:24 +0000

    add resnik aws experiments

--- tools/bin/aws/run_experiments3.sh
+++ tools/bin/aws/run_experiments3.sh
@@ -5,14 +5,20 @@ cd ~/src/nt-recommend/src
 
 # resnik collaborative filtering
 
-python3 manage.py resnik_collaborative_filter  --m msd --ns 10 --kn 1000
+#python3 manage.py resnik_collaborative_filter  --m msd --ns 10 --kn 1000
+#sleep 10
+#
+#python3 manage.py resnik_collaborative_filter  --m msd --ns 100 --kn 1000
+#sleep 10
+#
+#python3 manage.py resnik_collaborative_filter  --m pearson --ns 10 --kn 1000
+#sleep 10
+#
+#python3 manage.py resnik_collaborative_filter  --m pearson --ns 100 --kn 1000
+#sleep 10
+
+python3 manage.py resnik_collaborative_filter  --m msd --ns 1000 --kn 10000
 sleep 10
 
-python3 manage.py resnik_collaborative_filter  --m msd --ns 100 --kn 1000
-sleep 10
-
-python3 manage.py resnik_collaborative_filter  --m pearson --ns 10 --kn 1000
-sleep 10
-
-python3 manage.py resnik_collaborative_filter  --m pearson --ns 100 --kn 1000
+python3 manage.py resnik_collaborative_filter  --m pearson --ns 1000 --kn 10000
 sleep 10
