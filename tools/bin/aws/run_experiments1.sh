238ff86c2de71221dde54e152ba3543d7e5cc728
Manuel Ignacio Franco Galeano <maigfrga@gmail.com>
2017-11-29 09:42:13 +0000

    aws resnik

--- tools/bin/aws/run_experiments1.sh
+++ tools/bin/aws/run_experiments1.sh
@@ -4,18 +4,26 @@ cd ~/src/nt-recommend/src
 
 # leave one out
 
-python3 manage.py mean_predictor
+#python3 manage.py mean_predictor 
+#sleep 10
+#
+#
+#python3 manage.py collaborative_filter --m msd --ns 100
+#sleep 10
+#
+#python3 manage.py collaborative_filter --m pearson --ns 100
+#sleep 10
+#
+#python3 manage.py resnik_collaborative_filter --m msd --ns 100
+#sleep 10
+#
+#python3 manage.py resnik_collaborative_filter --m pearson --ns 100
+#sleep 10
+python3 manage.py mean_predictor --kn 10
 sleep 10
 
-
-python3 manage.py collaborative_filter --m msd --ns 100
+python3 manage.py mean_predictor --kn 1000
 sleep 10
 
-python3 manage.py collaborative_filter --m pearson --ns 100
-sleep 10
-
-python3 manage.py resnik_collaborative_filter --m msd --ns 100
-sleep 10
-
-python3 manage.py resnik_collaborative_filter --m pearson --ns 100
+python3 manage.py mean_predictor --kn 10000
 sleep 10
